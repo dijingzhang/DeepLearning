72386be77984ff62e22e3820329ef5c3c1d12fde
dijingzhang <78770000+dijingzhang@users.noreply.github.com>
2021-06-14 16:48:26 -0400

    Delete test.py

--- CNN/base/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-W = np.random.random((4, 5, 3))
-
-W_flip = np.zeros_like(W)
-for i in range(W.shape[0]):  # W=[out_channel, in_channel, kernel_size]
-    W_flip[i, :, :] = np.fliplr(W[i, :, :])
-    print(W_flip[i, :, :])
-    print(W[i, :, :])
-    break

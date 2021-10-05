f37fb76b02992ff91aaae547b0ac261ac8b90679
dijingzhang <78770000+dijingzhang@users.noreply.github.com>
2021-10-05 13:10:25 -0400

    Update mixup_learning.py

--- Tricks/mixup_learning.py
+++ Tricks/mixup_learning.py
@@ -5,7 +5,7 @@ def mixup_learning(alpha, images, target, model, criterion):
     lamda = np.random.beta(alpha, alpha)
     index = torch.randperm(images.size(0)).cuda()
     inputs = lamda * images + (1 - lamda) * images[index, :]
-    target_a, target_b = target, target[index]
-    outputs = model(inputs)
-    loss = lamda * criterion(outputs, target_a) + (1 - lamda) * criterion(outputs, target_b)
+    target_mixup = lamda * target + (1 - lamda) * target[index]
+    outputs = model(inputs) 
+    loss = criterion(outputs, target_mixup)
     return loss
